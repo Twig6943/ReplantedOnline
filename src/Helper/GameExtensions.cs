ecff9b464c5cd6b761f0cebc087bb6caabc07431
D1GQ <yourboylax@gmail.com>
2025-11-05 06:30:03 -0600

    add utils

---
+++ src/Helper/GameExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Il2CppReloaded.Utils;
+
+namespace ReplantedOnline.Helper;
+
+/// <summary>
+/// Provides extension methods for game-specific types to simplify common operations
+/// in multiplayer scenarios.
+/// </summary>
+internal static class GameExtensions
+{
+    /// <summary>
+    /// Gets the local player's item from a multiplayer collection.
+    /// </summary>
+    /// <typeparam name="T">The type of items in the multiplayer collection.</typeparam>
+    /// <param name="multiplayerType">The multiplayer collection instance.</param>
+    /// <returns>The item associated with the local player.</returns>
+    internal static T LocalItem<T>(this MultiplayerType<T> multiplayerType)
+    {
+        return multiplayerType[ReplantedOnlineMod.Constants.LOCAL_PLAYER_INDEX];
+    }
+
+    /// <summary>
+    /// Gets the opponent player's item from a multiplayer collection.
+    /// </summary>
+    /// <typeparam name="T">The type of items in the multiplayer collection.</typeparam>
+    /// <param name="multiplayerType">The multiplayer collection instance.</param>
+    /// <returns>The item associated with the opponent player.</returns>
+    internal static T OpponentItem<T>(this MultiplayerType<T> multiplayerType)
+    {
+        return multiplayerType[ReplantedOnlineMod.Constants.OPPONENT_PLAYER_INDEX];
+    }
+}
--- src/Helper/Il2CppExtensions.cs
+++ src/Helper/Il2CppExtensions.cs
@@ -21,4 +21,72 @@ internal static class Il2CppExtensions
     {
         unityEvent.AddListener(action);
     }
+
+    public static Il2CppSystem.Collections.Generic.List<T> ToIl2CppList<T>(this List<T> managedList)
+    {
+        var il2cppList = new Il2CppSystem.Collections.Generic.List<T>();
+        foreach (var item in managedList)
+            il2cppList.Add(item);
+        return il2cppList;
+    }
+
+    public static List<T> ToManagedList<T>(this Il2CppSystem.Collections.Generic.List<T> il2cppList)
+    {
+        var managedList = new List<T>();
+        for (int i = 0; i < il2cppList.Count; i++)
+            managedList.Add(il2cppList[i]);
+        return managedList;
+    }
+
+    public static T[] ToIl2CppArray<T>(this T[] managedArray)
+    {
+        if (managedArray == null) return null;
+
+        var il2cppArray = new T[managedArray.Length];
+        for (int i = 0; i < managedArray.Length; i++)
+            il2cppArray[i] = managedArray[i];
+        return il2cppArray;
+    }
+
+    public static T[] ToManagedArray<T>(this T[] il2cppArray)
+    {
+        if (il2cppArray == null) return null;
+
+        var managedArray = new T[il2cppArray.Length];
+        for (int i = 0; i < il2cppArray.Length; i++)
+            managedArray[i] = il2cppArray[i];
+        return managedArray;
+    }
+
+    public static T[] ToIl2CppArray<T>(this List<T> managedList)
+    {
+        if (managedList == null) return null;
+
+        var il2cppArray = new T[managedList.Count];
+        for (int i = 0; i < managedList.Count; i++)
+            il2cppArray[i] = managedList[i];
+        return il2cppArray;
+    }
+    public static T[] ToIl2CppArray<T>(this Il2CppSystem.Collections.Generic.List<T> il2cppList)
+    {
+        if (il2cppList == null) return null;
+
+        var il2cppArray = new T[il2cppList.Count];
+        for (int i = 0; i < il2cppList.Count; i++)
+            il2cppArray[i] = il2cppList[i];
+        return il2cppArray;
+    }
+
+    public static T[] ToIl2CppArray<T>(this IEnumerable<T> enumerable)
+    {
+        if (enumerable == null) return null;
+
+        if (enumerable is List<T> list)
+            return list.ToIl2CppArray();
+        if (enumerable is T[] array)
+            return array.ToIl2CppArray();
+
+        var tempList = new List<T>(enumerable);
+        return tempList.ToIl2CppArray();
+    }
 }
